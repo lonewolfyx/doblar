dafcdbbb31155ef3f2030885530cb6061ac8cc91
lonewolfyx <877085893@qq.com>
2025-05-09 01:06:54 +0800

    init

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+import { version } from '../package.json'
+import cac from 'cac'
+import chalk from 'chalk'
+import { devCommand } from '@/commands/dev.ts'
+
+const cli = cac('doblar')
+
+// 版本信息
+cli.version(version)
+cli.help()
+
+// 创建应用命令
+cli
+    .command('create', '创建新应用')
+    .action(async () => {
+        console.log(chalk.blue('开始创建应用...'))
+        // await createCommand();
+    })
+
+// 开发调试命令
+cli
+    .command('dev', '启动开发服务器')
+    .option('--port <port>', '指定端口号', { default: 3000 })
+    .action(async (options) => {
+        console.log(chalk.blue('开始创建调试应用...'), options)
+        await devCommand(options)
+    })
+
+// 解析命令行参数
+cli.parse(process.argv)
+
+// 如果没有提供命令，显示帮助信息
+if (!process.argv.slice(2).length) {
+    cli.outputHelp()
+}
